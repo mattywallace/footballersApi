799bba9fd79062f0d227d4cc5e3e5dcce7e52c38
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-04-28 18:42:23 -0500

    cheeky footballers now renders in first get route

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ PORT=8000
 
 app = Flask(__name__) # instantiateing the flask class 
 
+# these are where the routes go
+# the first should always be, by convention, a hello world 
+
+@app.route('/')
+def hello():
+	return "Cheeky Footballers"
+
 if __name__ == '__main__':
 	app.run(debug=DEBUG, port=PORT)
