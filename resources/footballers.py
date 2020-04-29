91ea2504e8aff77c6edb35049652dc37e61b3963
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-04-28 20:56:10 -0500

    hit the create footballer route

--- resources/footballers.py
+++ resources/footballers.py
@@ -4,12 +4,12 @@ from flask import Blueprint, request
 
 footballers = Blueprint('footballers', 'footballers')
 
-@footballers.route('/')
+@footballers.route('/', methods=['GET'])
 def footballers_index():
 		return 'footballers resource now working'
 
-# @footballers.route('/', methods=['POST'])
-# def creat_footballer():
-# 	payload = request.get_json()
-# 	print(payload)
-# 	return 'you hit the create footballer route -- cheack terminal'
+@footballers.route('/', methods=['POST'])
+def creat_footballer():
+	payload = request.get_json()
+	print(payload)
+	return 'you hit the create footballer route -- cheack terminal'
