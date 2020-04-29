aef67e7fc5d00e336ca4dab65b3e39560e1e0c79
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-04-28 21:57:04 -0500

    the file is telling me i have a none type error cannont resolve

--- resources/footballers.py
+++ resources/footballers.py
@@ -1,15 +1,47 @@
 import models 
-
 from flask import Blueprint, request
 
+
 footballers = Blueprint('footballers', 'footballers')
+
+
+
+
 
 @footballers.route('/', methods=['GET'])
 def footballers_index():
 		return 'footballers resource now working'
 
+
+
+
 @footballers.route('/', methods=['POST'])
-def creat_footballer():
+def create_footballer():
+	
 	payload = request.get_json()
-	print(payload)
-	return 'you hit the create footballer route -- cheack terminal'
+	
+	print(payload,'here is the payload')
+	
+	new_footballer = models.Footballer.create(firstname=payload['first_name'])
+	
+	print(new_footballer, 'here is the new footballer')
+	
+	return 'you hit the create route'
+	
+	
+
+	# 	last_name=payload['last_name'], 
+	# 	nationality=payload['nationality'], 
+	# 	position=payload['position'], 
+	# 	age=payload['age'], 
+	# 	club_team=payload['club_team'], 
+	# 	number=payload['number'])
+
+	# print(new_footballer)
+	# print(dir(new_footballer))
+	# footballer_dict = model_to_dict(new_footballer)
+	# return jsonify (
+	# 	data=footballer_dict,
+	# 	message='succussefully created footballer!',
+	# 	status=201
+	# 	),201
